944d46af5956432a8407c802a6e6681995173474
francocelan <francocelan2016@gmail.com>
2019-12-20 00:00:25 -0300

    perfil usuario franco

---
+++ resources/views/perfil-user.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center mt-5">
+    	<div class="col-md-4 text-center">
+    		<img src="/img/user.png" alt="" class="img-fluid">
+    		<div class="row">
+    			<div class="col-md-12 mt-5">
+    				<h1>Sebastian</h1>
+    			</div>
+    		</div>
+    		
+    	</div>
+        <div class="col-md-8">
+            <h5>Editar Perfil</h5>
+            <div class="row">
+            	<div class="col-md-12">
+            		<div class="card">
+					  <div class="card-header">Header</div>
+					  <div class="card-body">Content</div>
+
+					</div>
+            	</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
